292ecb211013acaa5663b0cbd1f1c5062f6e92db
Monir Hossain <monirhdigital@gmail.com>
2025-08-31 02:27:05 +0600

    frontend private route update

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,17 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {userStore} from "../store/userStore.js";
-
-// Navigate allows redirection to a different route
 import {Navigate, Outlet} from 'react-router-dom';
-
-// For accessing stored token in cookies
 import cookies from "js-cookie";
 
-// Helper function to extract role from token or local data
-// import { getRole } from "../helpers/helper.js";
 
-
-// PrivateRoute component protects routes based on user authentication and role
 const PrivateRoute = ({ allowedRoles }) => {
     const { role} = userStore();
     const token = cookies.get('accessToken');
