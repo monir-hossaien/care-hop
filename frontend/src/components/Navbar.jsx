102a85630317a099066499216b2971d9c0a92f8b
Monir Hossain <monirhdigital@gmail.com>
2025-09-03 23:34:26 +0600

    frontend base url update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {Link, useLocation, useNavigate} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import {FaUser} from "react-icons/fa";
 import {FaBarsStaggered} from "react-icons/fa6";
 import {AiOutlineClose} from "react-icons/ai";
